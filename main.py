9ae7792c16f4a5c9a58755fe26e8713b420c9250
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2026-02-27 14:54:02 +0100

    Adding the MCP Client with LangGraph agent logic

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
@@ -21,11 +22,18 @@ llm = ChatOpenAI(**LLM_CONFIG)
 
 stdio_server_params = StdioServerParameters(
     command="python",
-    args=["/home/eslam/ai_projects/mcp-servers/mcp-crash-course/servers/weather_server.py"],
+    args=["/home/eslam/ai_projects/mcp-servers/mcp-crash-course/servers/math_server.py"],
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session Initialized")
+            tools = await load_mcp_tools(session)
 
+            agent = create_agent(llm,tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 62 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 if __name__ == "__main__":
     asyncio.run(main())

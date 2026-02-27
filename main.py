3464336c7d6c04e8dd85e06c2132c3e4d0eaf157
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2026-02-27 03:14:54 +0100

    Enhance main application with LLM integration and update dependencies
    - Integrated ChatOpenAI with configuration for OpenRouter API in main.py
    - Added stdio server parameters for weather server execution
    - Updated pyproject.toml to include isort as a dependency
    - Modified uv.lock to reflect new isort package details
    - Added a blank line in weather_server.py for consistency

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
+
+LLM_CONFIG = {
+    "base_url": "https://openrouter.ai/api/v1",
+    "api_key": os.getenv("OPENROUTER_API_KEY"),
+    "model": "gpt-5-nano",
+    "temperature": 0,
+}
+
+llm = ChatOpenAI(**LLM_CONFIG)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/home/eslam/ai_projects/mcp-servers/mcp-crash-course/servers/weather_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
